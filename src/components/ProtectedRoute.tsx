e1ef99890473e53082801a5dd16b15fb40655570
Durga-kondaveeti <durgashankarkondaveeti@gmail.com>
2025-08-07 19:14:34 -0400

    setup Auth context

---
+++ src/components/ProtectedRoute.tsx
@@ -0,0 +1,14 @@
+// src/components/ProtectedRoute.tsx
+import { Navigate } from "react-router-dom";
+import { useAuth } from "../contexts/AuthContext";
+import type { JSX } from "react/jsx-dev-runtime";
+
+export default function ProtectedRoute({ children }: { children: JSX.Element }) {
+  const { user } = useAuth();
+
+  if (!user) {
+    return <Navigate to="/login" />;
+  }
+
+  return children;
+}
